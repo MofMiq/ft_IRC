5874c668ae28ddc964017a6d46f8b554858c7c84
Marina Alba Rodriguez Estebanez <marirodr@c1r14s2.42malaga.com>
2024-06-10 15:53:26 +0200

    first merge mal

--- inc/User.hpp
+++ inc/User.hpp
@@ -1,16 +1,28 @@
 #pragma once
 
-#include "Server.hpp"
-
+#include <string>
+//#include "Server.hpp"
 class User
 {
-    public:
-        std::string _username;
-        std::string _nickname;
-        std::string _hostname;
-        int fd; // -> para el poll
-        //contenedor para canal????
+private:
+    std::string _userName;
+    std::string _nickName;
+    std::string _hostName;
+    int _fd; // File descriptor para el poll
 
-        //constructores&Destructores
-        //getter&setter -> NICK & USER
+public:
+    User(int fd);
+    ~User();
+
+    // Getters y setters
+    std::string getUsername() const;
+    void setUsername(const std::string& username); //Pa el comando USER
+
+    std::string getNickname() const;
+    void setNickname(const std::string& nickname); //PA el comando NICK
+
+    std::string getHostname() const;
+    void setHostname(const std::string& hostname);
+
+    int getFd() const;
 };
